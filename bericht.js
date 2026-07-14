b8a49d26bc10594cfe14c6a2845619d160cba301
iki1uc <entertrainer@web.de>
2026-07-14 05:23:31 +0200

    Update bericht.js

--- bericht.js
+++ bericht.js
@@ -1,1 +1,30 @@
+// bericht.js – Globaler Wartungsbericht
+
+import { scanSystems } from "./scan.js";
+import { markError } from "./fehler.js";
+import { markTmp } from "./tmp.js";
+
+export function wartungsBericht(){
+
+    const systems = scanSystems();
+
+    const fehler = [
+        markError("LEGO/old_core.js"),
+        markError("GO/tmp_scan.js"),
+        markError("CLOCK/time_old.js")
+    ];
+
+    const tmp = [
+        markTmp("LE/tmp"),
+        markTmp("RIZ/tmp"),
+        markTmp("TET/tmp")
+    ];
+
+    return {
+        systems,
+        fehler,
+        tmp,
+        status: "WARTUNG-AKTIV"
+    };
+}
 
