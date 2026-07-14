7a54a4cb24f89f4890f751ba62a7869c6984eabf
iki1uc <entertrainer@web.de>
2026-07-14 05:22:54 +0200

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,1 +1,37 @@
+// scan.js – System-Scanner
 
+export const SYSTEMS = [
+    "LE",
+    "LEGO",
+    "GO",
+    "CLOCK",
+    "TET",
+    "RIZ",
+    "HOME",
+    "lage",
+    "octa²",
+    "octa³",
+    "698869",
+    "SET",
+    "IN",
+    "MROR",
+    "3TH",
+    "RUN8",
+    "O12",
+    "SYS",
+    "TEM",
+    "ID",
+    "OP",
+    "3hit90",
+    "SETUP",
+    "ROOT"
+];
+
+export function scanSystems(){
+    return SYSTEMS.map(name => ({
+        name,
+        status: "SCAN",
+        error: false,
+        tmp: false
+    }));
+}
